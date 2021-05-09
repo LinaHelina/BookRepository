b2b080c79d1223c2733f11372d0ece4097c8b2a9
AngelSel <angelbox1@gmail.com>
2021-05-09 15:26:49 +0300

    Project files added

---
+++ WebApplication2/WebApplication2/Entities/Configs/CategoryConfig.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using WebApplication2.Models;
+
+
+namespace WebApplication2.Entities.Configs
+{
+    internal sealed class CategoryConfig : IEntityTypeConfiguration<CategoryModel>
+    {
+        public void Configure(EntityTypeBuilder<CategoryModel> entity)
+        {
+            entity.ToTable(nameof(CategoryModel));
+
+            entity.HasKey(x => x.CategoryId);
+            entity.Property(x => x.CategoryId).ValueGeneratedNever();
+
+            entity.Property(x => x.CategoryName).IsRequired().HasMaxLength(64);
+            
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Entities/Models/BookModel.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApplication2.Models
+{
+    public class BookModel
+    {
+        public int BookId { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+        public string Language { get; set; }
+        public string PublicationDate { get; set; }
+        public string Cover { get; set; }
+        
+    }
+}
