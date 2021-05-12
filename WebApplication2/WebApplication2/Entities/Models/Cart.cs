0b505c6b84a7d891ef8c3cf955e114a6614964d1
AngelSel <angelbox1@gmail.com>
2021-05-12 11:15:18 +0300

    Cart class added

---
+++ WebApplication2/WebApplication2/Entities/Models/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Cart
+    {
+        [Key]
+        public int CartId { get; set; }
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductImageUrl { get; set; }
+        public string ProductAuthor { get; set; }
+        public int quantity { get; set; }
+        public double ProductPrice { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
