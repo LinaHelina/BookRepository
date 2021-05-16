ef35e999a267b99f76711b7c748cc6701ce2be16
AngelSel <angelbox1@gmail.com>
2021-05-16 11:11:38 +0300

    Admin role migration aded

---
+++ WebApplication2/WebApplication2/Migrations/20210516080301_AddAdminColumn.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
+
+namespace WebApplication2.Migrations
+{
+    public partial class AddAdminColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(name: "Role", table: "Customers", nullable: false,
+                defaultValue: 2);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
